39cc01d6a18cb5bf97aa7e8fcdf6e6858630e0b5
Michael2512 <MichaelNunhofer@t-online.de>
2014-07-06 18:32:21 +0200

    Anfang der Implementierung der Library

--- src/lib/Karte.java
+++ src/lib/Karte.java
@@ -1,5 +1,47 @@
+/**
+ * Eine Klasse, die eine Karte im Spiel repräsentiert.
+ * Der Wert der Karte kann nicht geändert werden, da es
+ * sich um ein Kartendeck handelt.
+ * @author michael
+ *
+ */
+
 package lib;
 
 public class Karte {
+	
+	//Das Enum legt für jeden Wert einen Integer-Wert fest, d.h. : EICHEL = 1
+	public enum farbe {EICHEL, GRAS, HERZ, SCHELLEN};
+	public enum wert {NEUN, ZEHN, KONIG, SAU, UNTER, OBER};
+	
+	private farbe f;
+	
+	private wert w;
+	
+	/**
+	 * Jede Karte muss eine Farbe und einen Wert haben
+	 * @param f
+	 * @param w
+	 */
+	public Karte(farbe f, wert w) {
+		this.f = f;
+		this.w = w;
+	}
+	
+	/**
+	 * Gibt die Farbe der Karte zurück
+	 * @return Farbe
+	 */
+	public farbe gibFarbe() {
+		return f;
+	}
+	
+	/**
+	 * Gibt den Wert der Karte zurück
+	 * @return Wert
+	 */
+	public wert gibWert() {
+		return w;
+	}
 
 }
--- src/lib/Model.java
+++ src/lib/Model.java
@@ -1,5 +1,27 @@
+/**
+ * Klasse, die den Aktuellen Spielstand beherbergt.
+ * Dazu zählen alle Karten auf dem Tisch, die Karten
+ * aller Spieler und eine Liste aller Karten.
+ */
+
 package lib;
 
+import java.util.ArrayList;
+
 public class Model {
+	
+	//Alle Karten
+	private ArrayList<Karte> kartendeck;
+	
+	//Die Karten der einzelnen Spieler
+	private ArrayList< ArrayList<Karte> > spielerhande;
+	
+	//Alle Karten auf dem Tisch
+	private Karte[] tisch;
+	
+	public Model() {
+		
+	}
+	
 
 }
