9e582a276ca456eb085060821402c269cd8783da
Michael2512 <MichaelNunhofer@t-online.de>
2014-07-10 13:37:18 +0200

    Erster Servercode

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,19 @@
 package server;
 
 public class Server {
+        
+        //hält alle 4 Spieler, ob Bot oder Mensch
+        private Spieler[] spieler;
+        
+        //hält eine Verbindung zu einem Spieler, nötig um Spieler zu erstellen
+        private Netzwerk netzwerk;
+        
+        /**
+         * Erstellt einen neuen Server
+         **/
+        public Server() {
+              
+              spieler = new Spieler[4];
+        }
 
 }
