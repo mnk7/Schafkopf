dafe363309ee65b510b055f875307cf33efc64af
Q11BUL <Q11BUL@users.noreply.github.com>
2014-07-17 12:58:01 +0200

    Update Graphik.java

--- src/graphik/Graphik.java
+++ src/graphik/Graphik.java
@@ -3,12 +3,39 @@ package graphik;
 import client.ModelMVC;
 import client.View;
 
-public class Graphik implements View {
-
+public class Graphik implements View extends JFrame
+{
+	public JButton start;     //Startknopf
+	private JTextField spieler1;  //Name Spieler 1
+	private JTextField spieler2;  //Name Spieler 2
+	private JTextField spieler3;  //Name Spieler 3
+	private JTextField spieler4;  //Name Spieler 4
+	private JLabel score1;  //Spielstand Spieler1
+	private JLabel score2;  //Spielstand Spieler2
+	private JLabel score3;  //Spielstand Spieler3
+	private JLabel score4;  //Spielstand Spieler4
+	private JLabel hintergrund; //Hintergrund (optional)
+	private JLabel anzeige;  //zeigt aktuelles Spiel an
+	
+	public Graphik
+	{
+		start = new JButton();
+		spieler1 = new JTextField();
+		
+	}
+	}
+	
+	
+	
+	
 	@Override
-	public void update(ModelMVC model) {
+	public void update(ModelMVC model)
+	{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
+	
 
 }
