1ad523e3db8d27f78004d283e854dbc25aed5511
Michael2512 <MichaelNunhofer@t-online.de>
2014-07-17 12:34:24 +0200

    Menüklasse

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,4 +2,16 @@ package client;
 
 public class Client implements View {
 
+	private Netzwerk netzwerk;
+	
+	private ModelMVC model;
+	
+	public Client() {
+		
+	}
+	
+	public void update(ModelMVC model) {
+		
+	}
+
 }
---
+++ src/graphik/MenuGUI.java
@@ -0,0 +1,19 @@
+package graphik;
+
+import javax.swing.JFrame;
+
+public class MenuGUI extends JFrame{
+
+	/**
+	 * Startet den Client und die Graphik
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MenuGUI menu = new MenuGUI();
+	}
+	
+	public MenuGUI() {
+		
+	}
+
+}
