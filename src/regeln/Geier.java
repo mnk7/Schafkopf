7338c60f36a27a92bfd46f9f2b6f83888bf299bd
Q11BUL <Q11BUL@users.noreply.github.com>
2014-07-17 12:56:45 +0200

    Update Geier.java
    attribute + constructor

--- src/regeln/Geier.java
+++ src/regeln/Geier.java
@@ -1,6 +1,11 @@
 package regeln;
-
+import java.util.ArrayList;
 
 public class Geier implements Controll {
-
+private Model m;
+private ArrayList<Karte> spielbar;
+public Geier(Model mNeu){
+  m = mNeu;
+  spielbar = = new ArrayList<Karte>();
+}
 }
