c3343030e31c85629fd5b3623ad1a39b1fbd2920
Q11BUL <Q11BUL@users.noreply.github.com>
2014-07-17 12:56:47 +0200

    Update Wenz.java
    Attribute + Constructor

--- src/regeln/Wenz.java
+++ src/regeln/Wenz.java
@@ -1,6 +1,13 @@
 package regeln;
-
+import.java.util.ArrayList;
 
 public class Wenz implements Controll {
 
+    private Model m;
+    private ArrayList<Karte> spielbar;
+    public Wenz(Model mNeu){
+      m = mNeu;
+      spielbar = new ArrayList<Karte> ();
+    }
+    
 }
